90f2d08172dd3fa0942bc327b3499a9177bb7c0f
MinboyKim <kdm3801@gmail.com>
2025-01-14 22:41:12 +0900

    feat: Oauth test

--- src/features/login/components/OauthCallback.tsx
+++ src/features/login/components/OauthCallback.tsx
@@ -1,3 +1,10 @@
+import { useLocation } from 'react-router-dom';
+
 export function OauthCallback() {
+  const location = useLocation();
+  const hashWithoutHash = location.hash.split('#')[1] || '';
+
+  console.log(hashWithoutHash);
+
   return <div>OauthCallback</div>;
 }

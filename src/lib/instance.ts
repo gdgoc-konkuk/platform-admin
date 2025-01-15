eff5b009d423126da2011c2d2337c6f6664379dd
MinboyKim <kdm3801@gmail.com>
2025-01-15 22:42:04 +0900

    fix: refresh error

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -23,6 +23,7 @@ instance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response.status === 401) {
+      localStorage.removeItem('token');
       window.location.href = '/login';
     }
     return Promise.reject(error);
